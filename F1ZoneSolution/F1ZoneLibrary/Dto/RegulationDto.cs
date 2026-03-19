0d885a1f05e85dc9d46819ccf671c4e79d7e9c4a
beszelaszlohihi <besze.laszlo@ganziskola.hu>
2026-03-19 23:55:51 +0100

    0319 szabalyvaltozasok oldal, illetve a szinek

---
+++ F1ZoneSolution/F1ZoneLibrary/Dto/RegulationDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1ZoneLibrary.Dto
+{
+    public class RegulationDto
+    {
+        public int Season_year { get; set; }
+        public string Technical_regulations { get; set; }
+    }
+}
