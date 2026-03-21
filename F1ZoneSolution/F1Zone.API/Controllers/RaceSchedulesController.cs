2cb17198da6bed2a18a6cb2b2140b05d4a8ea525
beszelaszlohihi <besze.laszlo@ganziskola.hu>
2026-03-21 01:22:37 +0100

    0321 versenynaptar oldal

---
+++ F1ZoneSolution/F1Zone.API/Controllers/RaceSchedulesController.cs
@@ -0,0 +1,14 @@
+﻿using F1Zone.API.INTERFACE;
+using F1ZoneLibrary.MODEL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace F1Zone.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RaceSchedulesController : GenericController<RaceSchedules>
+    {
+        public RaceSchedulesController(IGenericF1ZoneService<RaceSchedules> service) : base(service) { }
+    }
+}
--- F1ZoneSolution/F1Zone.API/obj/Debug/net8.0/F1Zone.API.AssemblyInfo.cs
+++ F1ZoneSolution/F1Zone.API/obj/Debug/net8.0/F1Zone.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("F1Zone.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d28da3783ec6e86ce337834207190e4337f4799c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("F1Zone.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("F1Zone.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
