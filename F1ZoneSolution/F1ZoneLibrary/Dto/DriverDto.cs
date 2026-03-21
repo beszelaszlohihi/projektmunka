16f4f488c09c49d3013e528922411d23c830ee72
beszelaszlohihi <besze.laszlo@ganziskola.hu>
2026-03-21 18:48:09 +0100

    0321

--- F1ZoneSolution/F1ZoneLibrary/Dto/DriverDto.cs
+++ F1ZoneSolution/F1ZoneLibrary/Dto/DriverDto.cs
@@ -1,24 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace F1ZoneLibrary.MODEL
+namespace F1ZoneLibrary.Dto
 {
-    public class Drivers
+    public class DriverDto
     {
-        [Key]
         public int driver_id { get; set; }
-        public string driver_name { get; set; }
-        public string nationality { get; set; }
+        public string driver_name { get; set; } = "";
+        public string nationality { get; set; } = "";
         public int debut_year { get; set; }
+        public string biography { get; set; } = "";
+
+        
         public int championships { get; set; }
-        public string biography { get; set; }
         public int wins { get; set; }
         public int podiums { get; set; }
-        public int fastest_laps{ get; set; }
+        public int fastest_laps { get; set; }
         public decimal points { get; set; }
     }
 }
