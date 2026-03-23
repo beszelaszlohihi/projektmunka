d27c9c2404c97fb45362912cbe0c163cd3a22fad
beszelaszlohihi <besze.laszlo@ganziskola.hu>
2026-03-23 01:29:21 +0100

    0323 admin oldal, edit oldal ilyesmik

--- F1ZoneSolution/F1ZoneLibrary/Dto/DriverDto.cs
+++ F1ZoneSolution/F1ZoneLibrary/Dto/DriverDto.cs
@@ -2,23 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace F1ZoneLibrary.Dto
 {
     public class DriverDto
     {
+        [JsonPropertyName("driver_id")]
         public int driver_id { get; set; }
+        [JsonPropertyName("driver_name")]
         public string driver_name { get; set; } = "";
         public string nationality { get; set; } = "";
         public int debut_year { get; set; }
         public string biography { get; set; } = "";
-
-        
+        public string TeamName { get; set; }
+        [JsonPropertyName("championships")]
         public int championships { get; set; }
+        [JsonPropertyName("wins")]
         public int wins { get; set; }
         public int podiums { get; set; }
         public int fastest_laps { get; set; }
+        [JsonPropertyName("points")]
         public decimal points { get; set; }
     }
 }
