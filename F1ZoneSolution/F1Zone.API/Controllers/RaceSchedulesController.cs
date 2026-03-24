ccdfec28bc64950f8e87ea3e273e89ce1ef17cbb
bartfai.levente <bartfai.levente@ganziskola.hu>
2026-03-24 20:52:38 +0100

    "service valtozasok"

--- F1ZoneSolution/F1Zone.API/Controllers/RaceSchedulesController.cs
+++ F1ZoneSolution/F1Zone.API/Controllers/RaceSchedulesController.cs
@@ -1,7 +1,8 @@
 ﻿using F1Zone.API.INTERFACE;
+using F1ZoneLibrary.DATA;
 using F1ZoneLibrary.MODEL;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace F1Zone.API.Controllers
 {
@@ -9,6 +10,28 @@ namespace F1Zone.API.Controllers
     [ApiController]
     public class RaceSchedulesController : GenericController<RaceSchedules>
     {
-        public RaceSchedulesController(IGenericF1ZoneService<RaceSchedules> service) : base(service) { }
+        private readonly F1ZoneDbContext _context;
+
+        public RaceSchedulesController(
+            IGenericF1ZoneService<RaceSchedules> service,
+            F1ZoneDbContext context
+        ) : base(service)
+        {
+            _context = context;
+        }
+
+        [HttpGet("next-race")]
+        public async Task<ActionResult<RaceSchedules>> GetNextRace()
+        {
+            var nextRace = await _context.RaceSchedules
+                .Where(r => r.RaceDate > DateTime.Now)
+                .OrderBy(r => r.RaceDate)
+                .FirstOrDefaultAsync();
+
+            if (nextRace == null)
+                return NotFound("Nincs közelgő futam.");
+
+            return Ok(nextRace);
+        }
     }
 }
