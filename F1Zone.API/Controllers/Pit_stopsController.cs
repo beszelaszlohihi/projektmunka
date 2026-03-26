a179f656cc9628ce19454e3f11e5b6f5c3e706d3
beszelaszlohihi <besze.laszlo@ganziskola.hu>
2026-03-26 01:00:36 +0100

    0326

---
+++ F1Zone.API/Controllers/Pit_stopsController.cs
@@ -0,0 +1,14 @@
+﻿using F1Zone.API.INTERFACE;
+using F1ZoneLibrary.MODEL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace F1Zone.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Pit_stopsController : GenericController<Pit_stops>
+    {
+        public Pit_stopsController(IGenericF1ZoneService<Pit_stops> service) : base(service) { }
+    }
+}
---
+++ F1Zone.API/Controllers/Team_sponsorsController.cs
@@ -0,0 +1,14 @@
+﻿using F1Zone.API.INTERFACE;
+using F1ZoneLibrary.MODEL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace F1Zone.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Team_sponsorsController : GenericController<Team_sponsors>
+    {
+        public Team_sponsorsController(IGenericF1ZoneService<Team_sponsors> service) : base(service) { }
+    }
+}
